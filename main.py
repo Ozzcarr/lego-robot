74a99611668ec67f69057c5dd18acb5b7193b713
Oscar Andersson <anderssonoscar03@gmail.com>
2023-04-04 15:26:52 +0200

    Squashed commit of the following:
    commit 10a6b3aa490f50c9cca33722cdd3ce2d1a81aa9f
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 4 15:17:52 2023 +0200
    
        Fixed imports
    
    commit dcc3047637c14f92a729913209341d824b46d32d
    Merge: e391364 54c8b91
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 4 15:01:09 2023 +0200
    
        Merge branch 'develop' of https://github.com/Ozzcarr/lego-robot into develop
    
    commit e391364bec47745a2c6e71320390ebd82a014aa9
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 4 14:56:54 2023 +0200
    
        Moved sensor initialization
    
    commit 54c8b911dba4635b54a72088360649545d8a8321
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 4 14:56:54 2023 +0200
    
        Moved sensor initialization
    
    commit 4085da1cf5135dcec928bf5d9639852a576ed3b7
    Merge: d7880fe 5e5e18d
    Author: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    Date:   Tue Apr 4 14:46:43 2023 +0200
    
        Merge branch 'develop' of https://github.com/Ozzcarr/lego-robot into develop
    
    commit d7880fee64722aaec2cd91f63047fec347cffc24
    Author: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    Date:   Tue Apr 4 14:41:45 2023 +0200
    
        Main base
    
    commit 5e5e18da52cc97447d198476f118fd2ab37a7908
    Author: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    Date:   Tue Apr 4 14:41:45 2023 +0200
    
        Main base
    
    commit ea7f9816c863f8a23220ef21d57ccc3167876683
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 4 14:00:18 2023 +0200
    
        Added initialization
    
    commit 0feed57711c2da1db0f96a139e507a5aba00cb5a
    Author: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    Date:   Tue Apr 4 13:23:05 2023 +0200
    
        added beep

--- main.py
+++ main.py
@@ -1,22 +1,88 @@
 #!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import ColorSensor, Motor, TouchSensor
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (
-    Motor,
-    TouchSensor,
-    ColorSensor,
-    InfraredSensor,
-    UltrasonicSensor,
-    GyroSensor,
-)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.parameters import Direction, Port, Stop
+from pybricks.tools import wait
 
 
-# Create your objects here.
+# Initialize the EV3 brick.
 ev3 = EV3Brick()
 
+# Initialization and configuration of motors
+gripper_motor = Motor(Port.A)
+elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
 
-# Write your program here.
-ev3.speaker.beep()
+# Initialize sensors
+touch_sensor = TouchSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
+
+# Define the three destinations for picking up and moving the wheel stacks.
+LEFT = 160
+MIDDLE = 100
+RIGHT = 40
+
+
+def calibration():
+    """Calibration of the sensors"""
+    # Limit acceleration
+    elbow_motor.control.limits(speed=60, acceleration=120)
+    base_motor.control.limits(speed=60, acceleration=120)
+
+    # Initialize the gripper
+    gripper_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+    gripper_motor.reset_angle(0)
+    gripper_motor.run_target(200, -90)
+
+    # Initialize the elbow
+    elbow_motor.run_time(-30, 1000)
+    elbow_motor.run(15)
+    while color_sensor.reflection() > 0:
+        wait(10)
+    elbow_motor.run_time(20, 500)
+    elbow_motor.reset_angle(0)
+    elbow_motor.hold()
+
+    # Initialize the base
+    base_motor.run(-60)
+    while not touch_sensor.pressed():
+        wait(10)
+    base_motor.reset_angle(0)
+    base_motor.hold()
+
+    # Play music to indicate that the initialization is complete.
+    the_lick = ["D4/8", "E4/8", "F4/8", "G4/8", "E4/4", "C4/8", "D4/4"]
+    triplets = ["C4/8", "C4/8", "C4/8", "C4/4"]
+    ending = ["Ab3/4", "Bb3/4", "C4/8.", "Bb3/16", "C4/4"]
+
+    ev3.speaker.play_notes(the_lick, tempo=160)
+    wait(1000)
+    ev3.speaker.play_notes(triplets, tempo=180)
+    wait(100)
+    ev3.speaker.play_notes(ending, tempo=150)
+
+
+def pickup(position):
+    """Pickup item at position"""
+    base_motor.run_target(60, position)
+    elbow_motor.run_target(60, -40)
+    gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
+    elbow_motor.run_target(60, 0)
+
+
+def release(position):
+    """Release item at position"""
+    base_motor.run_target(60, position)
+    elbow_motor.run_target(60, -40)
+    gripper_motor.run_target(200, -90)
+    elbow_motor.run_target(60, 0)
+
+
+def main():
+    """Main function"""
+    calibration()
+    # while True:
+
+
+if __name__ == "__main__":
+    main()
