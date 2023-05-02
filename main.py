fb2cc842e9eb200ee5d71323d048c8de9bbba4e2
Oscar Andersson <anderssonoscar03@gmail.com>
2023-05-02 14:15:31 +0200

    Squashed commit of the following:
    commit d9a91d1c0341fa5dfc198388d36a7fb45d6d125b
    Author: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    Date:   Wed Apr 26 23:41:11 2023 +0200
    
        Update main.py
    
    commit 12c247049032f7dd7ac8e35e5591e249fea4d7e5
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Mon Apr 24 16:47:51 2023 +0200
    
        Fixed mistake in set_location()
    
    commit bb1707461d1b839bbb9b6b309de91de2da2741e7
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Mon Apr 24 16:00:28 2023 +0200
    
        Too many changes, still not working
    
    commit 524db01dc7f5f8035ee3a24c6e530aaa7a10dd3d
    Author: Oscar Andersson <100211209+Ozzcarr@users.noreply.github.com>
    Date:   Sun Apr 23 19:03:20 2023 +0200
    
        US01B+US02B (#9)
    
        * update main func.
    
        * Changed main code to adapt to new function
    
        ---------
    
        Co-authored-by: Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
    
    commit b83e7b7ed7a0d93cfc2db233b3409fb9dc6b06d2
    Author: Oscar Andersson <100211209+Ozzcarr@users.noreply.github.com>
    Date:   Sun Apr 23 18:39:54 2023 +0200
    
        Added check item to pickup (#7)
    
        Co-authored-by: Remasproblemas <119594918+Remasproblemas@users.noreply.github.com>
    
    commit 2e184b022e783474af5c2081c8c7a67552bce978
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Sun Apr 23 18:36:03 2023 +0200
    
        Changed function since US03 was added
    
    commit 6886f69ebb6d16c0ed64469673e3c34454989239
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Tue Apr 18 17:20:55 2023 +0200
    
        Fixed mistake and added comment for uncertainty
    
    commit 9e178c7ed947dfc76e7fef010270672f93c66502
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Mon Apr 17 13:46:31 2023 +0200
    
        Fixed a mistake in the code
    
    commit 9db11cb70fed3102b5d0ae02f7203273ccd55dbb
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Sun Apr 16 16:10:16 2023 +0200
    
        Added function docstrings
    
    commit c810e9657ffdc57d3875c2328f639d6d48304cf6
    Merge: 1991c40 4601162
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Sun Apr 16 14:33:45 2023 +0200
    
        Merge branch 'US12' into develop
    
    commit 4601162a386a89e739e885911d4c2d1783de140c
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Sun Apr 16 14:17:35 2023 +0200
    
        Added functionality for US12 and US08B
    
    commit 1991c401a3d5a41b191a975515d6d064a26041d6
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Sun Apr 16 13:35:34 2023 +0200
    
        Fixed #5
    
    commit b931115a845c53dd4a4e63b1879677b625e0b7c6
    Merge: f88123b 9ce084b
    Author: Remasproblemas <119594918+Remasproblemas@users.noreply.github.com>
    Date:   Fri Apr 14 15:04:56 2023 +0200
    
        Merge branch 'develop' of https://github.com/Ozzcarr/lego-robot into develop
    
    commit f88123bd4e8ecc6828a63ec357799c960559e6fc
    Author: Remasproblemas <119594918+Remasproblemas@users.noreply.github.com>
    Date:   Wed Apr 12 13:56:33 2023 +0200
    
        Changed Music
    
    commit fb439eeeb66eac5262e1e1c29ef4f45ed41dd8b1
    Author: Remasproblemas <119594918+Remasproblemas@users.noreply.github.com>
    Date:   Wed Apr 12 13:25:12 2023 +0200
    
        Calibration
    
    commit f258417c9f5445e2d433e409b5ef0595c943c5d4
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Wed Apr 12 12:58:19 2023 +0200
    
        Added Run Until Stall
    
    commit 2b396529c84e7f3406665a58c7b977a5ea42ca84
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Wed Apr 12 12:57:30 2023 +0200
    
        Added Color Functions
    
    commit ffd6894bb1bfb7510126c26e9dce6637076e8227
    Author: Oscar Andersson <anderssonoscar03@gmail.com>
    Date:   Wed Apr 12 12:28:21 2023 +0200
    
        Added Color

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.ev3devices import ColorSensor, Motor, TouchSensor
 from pybricks.hubs import EV3Brick
-from pybricks.parameters import Direction, Port, Stop, Color
+from pybricks.parameters import Direction, Port, Stop, Color, Button
 from pybricks.tools import wait
 
 
@@ -17,10 +17,12 @@ base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
 touch_sensor = TouchSensor(Port.S1)
 color_sensor = ColorSensor(Port.S2)
 
-# Define the three destinations for picking up and moving the wheel stacks.
-LEFT = 160
-MIDDLE = 100
-RIGHT = 40
+# List for the pickup and drop off locations
+LOCATIONS = []
+
+# Define the colors
+COLOR_NAMES = {'#050d59': 'Blue', '#210808': 'Red', '#3d211c': 'Yellow', '#082121': 'Green'}
+COLORS = []
 
 
 def calibration():
@@ -35,6 +37,7 @@ def calibration():
     gripper_motor.run_target(200, -90)
 
     # Initialize the elbow
+    elbow_motor.run_until_stalled(-30, then=Stop.COAST, duty_limit=50)
     elbow_motor.run_time(-30, 1000)
     elbow_motor.run(15)
     while color_sensor.reflection() > 0:
@@ -57,45 +60,124 @@ def calibration():
     ev3.speaker.play_notes(nokia, tempo=200)
 
 
-
 def pickup(position):
-    """Pickup item at position"""
-    base_motor.run_target(60, position)
-    elbow_motor.run_until_stalled(-80, then=Stop.COAST, duty_limit=30)
+    """Checks if an item is at an location and pickup"""
+    base_motor.run_target(60, position[0])
+    elbow_motor.run_target(80, position[1], then=Stop.HOLD)
     gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
+
+    gripper_motor.hold()
     elbow_motor.run_target(60, 0)
+
+    if gripper_motor.angle() < -5:
+        return True
+    else:
+        gripper_motor.run_target(200, -90)
+        return False
 
 
 def release(position):
     """Release item at position"""
-    base_motor.run_target(60, position)
-    elbow_motor.run_until_stalled(-80, then=Stop.COAST, duty_limit=30)
+    base_motor.run_target(60, position[0])
+    elbow_motor.run_target(60, position[1], then=Stop.HOLD)
+    gripper_motor.run_target(200, -90)
+    elbow_motor.run_target(60, 40)
+
+
+def rgbp_to_hex(rgb):
+    """Returns the hex value of the color from the RGB percentage values"""
+    rgb = tuple(round(p * 2.55) for p in rgb)
+    return '#{:02x}{:02x}{:02x}'.format(*rgb)
+
+
+def diff(h1, h2):
+    """Returns how far apart two hex values are"""
+    def hex_to_int(s):
+        return [int(s[i:i+2], 16) for i in range(1,7,2)]
+    return sum(abs(i - j) for i, j in zip(*map(hex_to_int, (h1, h2))))
+
+
+def color_index(color):
+    """Returns the index to the closest color in COLORS"""
+    return COLORS.index(min([(c, diff(c, rgbp_to_hex(color))) for c in COLORS], key=lambda x: x[1])[0])
+
+
+def color_name(color):
+    """Returns the name of the color"""
+    return COLOR_NAMES[min([(c, diff(c, rgbp_to_hex(color))) for c, n in COLOR_NAMES.items()], key=lambda x: x[1])[0]]
+
+
+def ev3_light(color):
+    """Sets a color for the EV3 Brick light from color name"""
+    if color == 'Blue':
+        ev3.light.on(Color.ORANGE)
+    elif color == 'Red':
+        ev3.light.on(Color.RED)
+    elif color == 'Yellow':
+        ev3.light.on(Color.YELLOW)
+    elif color == 'Green':
+        ev3.light.on(Color.GREEN)
+
+
+def set_location():
+    """Returns the angles of the set position"""
+    while Button.CENTER not in ev3.buttons.pressed():
+        while Button.LEFT in ev3.buttons.pressed():
+            base_motor.run(50)
+        while Button.RIGHT in ev3.buttons.pressed():
+            base_motor.run(-50)
+        while Button.UP in ev3.buttons.pressed():
+            elbow_motor.run(30)
+        while Button.DOWN in ev3.buttons.pressed():
+            elbow_motor.run(-30)
+
+        base_motor.hold()
+        elbow_motor.hold()
+
+    elbow_angle = elbow_motor.angle()
     gripper_motor.run_target(200, -90)
     elbow_motor.run_target(60, 0)
+    return (base_motor.angle(), elbow_angle)
 
 
-def color():
-    return color_sensor.color()
+def set_locations():
+    """Set the pickup and drop off locations"""
+    ev3.screen.print("Set pickup location")
+    LOCATIONS.append(set_location())
+    set_more_locations = True
+    ev3.screen.print("Set drop-off locations")
+    while set_more_locations:
+        if Button.CENTER in ev3.buttons.pressed():
+            if pickup(LOCATIONS[0]):
+                COLORS.append(rgbp_to_hex(color_sensor.rgb()))
+                elbow_motor.run_target(60, 40)
+                ev3.screen.print(color_name(color_sensor.rgb()))
+                ev3_light(color_name(color_sensor.rgb()))
+                LOCATIONS.append(set_location())
+            else:
+                set_more_locations = False
 
 
 def main():
     """Main function"""
     calibration()
+    set_locations()
+    ev3.screen.print("Main Loop")
+    wait(5000)
 
-    rightColor = Color.RED
-    leftColor = Color.BLUE
+    if len(LOCATIONS) < 2:
+        return
 
     while True:
-        pickup(MIDDLE)
-        ev3.screen.clear()
-        ev3.screen.draw_text(40, 50, color())
-        if color() == rightColor:
-            release(RIGHT)
-        elif color() == leftColor:
-            release(LEFT)
-        else:
-            release(MIDDLE)
+        if not pickup(LOCATIONS[0]):
+            ev3.screen.print("No item")
+            wait(3000)
+            continue
 
+        color = color_sensor.rgb()
+        ev3.screen.print(color_name(color))
+        ev3_light(color_name(color))
+        release(LOCATIONS[color_index(color)+1])
 
 
 if __name__ == "__main__":
